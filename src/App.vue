5c93617249dd6bf12420c3b631cde0bdb2eaafda
ingrwf12 <ingrwf12@cepegra.be>
2025-08-27 14:54:12 +0200

    edit titre

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
+  <h1>Where are You ?</h1>
   <router-view></router-view>
 </template>
 
--- src/views/CarteView.vue
+++ src/views/CarteView.vue
@@ -3,5 +3,6 @@ import myCarte from '@/components/myCarte.vue'
 </script>
 
 <template>
+  <h1>Carte</h1>
   <myCarte></myCarte>
 </template>
